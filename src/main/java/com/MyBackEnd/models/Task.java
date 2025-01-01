dfc8ceeef2269c81f24b9bc58c9d6d25b7e62971
oussama-elhamraoui <oussama14elhamraoui@gmail.com>
2025-01-01 21:26:16 +0100

    doing some changes

--- src/main/java/com/MyBackEnd/models/Task.java
+++ src/main/java/com/MyBackEnd/models/Task.java
@@ -2,23 +2,14 @@ package com.MyBackEnd.models;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
@@ -32,6 +23,11 @@ public class Task {
     @JoinColumn(name = "project_id", nullable = false)
     @JsonIgnore
     private Project project;
+
+    // added this to insure the save deletion of a task
+    @OneToMany(mappedBy = "task", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @JsonIgnore
+    private List<TaskAssignment> taskAssignments;
 
     @Column(name = "name", nullable = false)
     private String name;
